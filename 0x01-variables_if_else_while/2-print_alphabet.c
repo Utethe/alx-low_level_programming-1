82a2c768f294885324354bc423734ba4c0ec3bb9
MaryBlessing <marybngozi@gmail.com>
2022-06-07 06:55:29 +0100

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the whole alphabets in loower case
+ * Return 0
+*/
+int main(void)
+{
+	char letters[26];
+
+	putchar();
+	return (0);
+}
