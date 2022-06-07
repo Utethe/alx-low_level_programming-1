5d2c29933666043996167d71f63dfdd22ef4d66d
MaryBlessing <marybngozi@gmail.com>
2022-06-07 09:04:59 +0100

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,8 @@ int main(void)
 	char hexs[16] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d',\
 			'e','f'};
 
-	for (int n = 0; n <= 16; ++n)
+	int n;
+	for (n = 0; n <= 16; ++n)
 	{
 		putchar(hexs[n]);
 	}
