e85c09c2716c464e6394262f96254e55e3e886a0
MaryBlessing <marybngozi@gmail.com>
2022-06-09 00:23:06 +0100

    3comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,17 +8,13 @@
 */
 int main(void)
 {
-	int n;
-	int st1 = 1;
-	int st2 = 2;
+	int n, k, l;
 
 	for (n = 0; n <= 9; n++)
 	{
-		int k;
-		for (k = st1; k <= 9; ++k)
+		for (k = n + 1; k <= 9; ++k)
 		{
-			int l;
-			for (l = st2; l <= 9; ++l)
+			for (l = k + 1; l <= 9; ++l)
 			{
 				putchar(n + '0');
 				putchar(k + '0');
@@ -30,9 +26,7 @@ int main(void)
 				putchar(',');
 				putchar(' ');
 			}
-			st2++;
 		}
-		st1++;
 	}
 	putchar('\n');
 	return (0);
