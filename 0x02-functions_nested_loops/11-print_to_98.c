6413ab2e4d21b2968986b9d4658ca1ebd3f2ab85
MaryBlessing <marybngozi@gmail.com>
2022-06-09 22:53:09 +0000

    times_table

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,21 +21,18 @@ void print_to_98(int n)
 
 		for (i = n; i <= 98; i++)
 		{
+			k = i;
 			if (i < 0)
 			{
 				_putchar('-');
 				k = abs(i);
 			}
-			else
-			{
-				k = i;
-			}
 			f = k / 10;
-			if (f / 10) 
+			if (f / 10)
 			{
 				_putchar(f / 10 + '0');
 			}
-			if (k / 10) 
+			if (k / 10)
 			{
 				_putchar(f % 10 + '0');
 				_putchar(k % 10 + '0');
@@ -44,7 +41,7 @@ void print_to_98(int n)
 			{
 				_putchar(k + '0');
 			}
-			if (k == 98)
+			if (i > 0 && k == 98)
 			{
 				continue;
 			}
@@ -60,7 +57,7 @@ void print_to_98(int n)
 		for (d = n; d >= 98; d--)
 		{
 			e = d / 10;
-			if (e / 10) 
+			if (e / 10)
 			{
 				_putchar(e / 10 + '0');
 			}
