04da3b7791aa9d69ca1273b4291eb6c4f4f695bd
MaryBlessing <marybngozi@gmail.com>
2022-06-09 16:43:06 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ *
+ * Description: prints the alphabet, in lowercase, followed by a new line
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return 1;
+	}
+	return 0;
+}
