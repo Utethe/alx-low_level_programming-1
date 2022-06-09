2aa20b4b4bc00515aa993f31b26971a8ea07b04a
MaryBlessing <marybngozi@gmail.com>
2022-06-09 15:08:34 +0000

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,31 +3,41 @@
 /**
  * main - Entry point
  *
- * Description: prints all possible different combinations of three digits
+ * Description: prints all possible combinations of two two-digit numbers.
  * Return:  0
 */
 int main(void)
 {
-	int n, k, l 10[;
+	int n, k, l, i;
 
 	for (n = 0; n <= 9; n++)
 	{
-		for (k = 0; k <= 9; ++k)
+		for (k = 0; k <= 9; k++)
 		{
-			for (l = 0; l <= 9; ++l)
+			for (l = 0; l <= 9; l++)
 			{
-				for (i = 0; i <= 9; ++i)
-					   r(k + '0');
-				putchar(l + '0');
-				if (n == 7 && k == 8 && l == 9)
+				for (i = 0; i <= 9; i++)
 				{
-					continue;
+					int nk = n * 10 + k;
+					int li = l * 10 + i;
+					if (nk < li)
+					{
+						putchar(n + '0');
+						putchar(k + '0');
+						putchar(32);
+						putchar(l + '0');
+						putchar(i + '0');
+						if (n == 9 && k == 8 && l == 9 && i == 9)
+						{
+							continue;
+						}
+						putchar(44);
+						putchar(32);
+					}
 				}
-				putchar(',');
-				putchar(' ');
 			}
 		}
 	}
-	putchar('\n');
+	putchar(10);
 	return (0);
 }
