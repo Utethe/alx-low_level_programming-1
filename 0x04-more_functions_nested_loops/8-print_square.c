eb26aee2bee6ed5f979502838e23e129a4bb2577
MaryBlessing <marybngozi@gmail.com>
2022-06-11 11:17:36 +0000

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - draws a diagonal line on the terminal
+ * @size: size of square
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int i, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= size; i++)
+	{
+		for (k = 1; k <= size; k++)
+		{
+			if (i == k)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
