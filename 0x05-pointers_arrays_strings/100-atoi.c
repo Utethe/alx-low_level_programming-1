41627a107db0bb665c4ba7fb24d716b0ab92678a
MaryBlessing <marybngozi@gmail.com>
2022-06-14 17:18:28 +0000

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,25 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-/**
- * append_digits - append an integer to another
- * @i1: integer 1
- * @i2: integer 2
- * Return: int
- */
-int append_digits(int i1, int i2)
-{
-	int i2_copy = i2;
-
-	while (i2_copy)
-	{
-		i1 *= 10;
-		i2_copy /= 10;
-	}
-	return (i1 + i2);
-}
-
 
 /**
  * _atoi - convert a string to an integer.
@@ -28,35 +7,5 @@ int append_digits(int i1, int i2)
  */
 int _atoi(char *s)
 {
-
-	int i, sn;
-	int num = 0;
-	int v = atoi(s);
-
-	i = 0;
-	sn = 1;
-	if (v > 0)
-	{
-		return (v);
-	}
-
-	for (i = 0; i < (int)strlen(s); i++)
-	{
-		if (s[i] == 43)
-		{
-			sn = sn * 1;
-		}
-		else if (s[i] == 45)
-		{
-			sn = sn * -1;
-		}
-		else if (isdigit(s[i]))
-		{
-			char c = s[i];
-			int a = atoi(&c);
-
-			num = append_digits(num, a);
-		}
-	}
-	return (sn * num);
+	return (atoi(s));
 }
