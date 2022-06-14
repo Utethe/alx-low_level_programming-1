54bd51af01450a33afb089c51ecec9057d9ecf25
MaryBlessing <marybngozi@gmail.com>
2022-06-14 16:50:25 +0000

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdio.h>
 
 /**
  * _atoi - convert a string to an integer.
@@ -10,32 +7,5 @@
  */
 int _atoi(char *s)
 {
-
-	int i, sn;
-	char num[12] = "0";
-	int v = atoi(s);
-
-	i = 0;
-	sn = 1;
-	if (v > 0)
-	{
-		return (v);
-	}
-
-	for (i = 0; i < (int)strlen(s); i++)
-	{
-		if (s[i] == 43)
-		{
-			sn = sn * 1;
-		}
-		else if (s[i] == 45)
-		{
-			sn = sn * -1;
-		}
-		else if (isdigit(s[i]))
-		{
-			strncat(num, &s[i], 1);
-		}
-	}
-	return (sn * atoi(num));
+	return (atoi(s));
 }
