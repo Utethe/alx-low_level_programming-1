591b6315e3b022a0a665c4256c45ab4bf51a6f58
MaryBlessing <marybngozi@gmail.com>
2022-06-16 16:23:15 +0000

    7-leet.c_ok

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,13 +6,12 @@
  */
 char *leet(char *str)
 {
-	char ch;
-	int nx, i;
+	int i;
 	int a = 0;
 	char z[] = {'a', 'e', 'o', 't', 'l'};
 	char d[] = {52, 51, 48, 55, 49};
 
-	for (a = 0; a < strlen(str); a++)
+	for (a = 0; a < (int)strlen(str); a++)
 	{
 		for (i = 0; i < 5; i++)
 		{
