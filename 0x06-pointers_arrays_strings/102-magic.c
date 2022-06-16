41ed271cd44e365ceb379a7a36d49b932fec3083
MaryBlessing <marybngozi@gmail.com>
2022-06-16 17:32:54 +0000

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,9 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  // *(p + 3) = 98;
-  printf("p: %p\n", p);
-  printf("a: %p\n", a);
+  *(a + 2) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
