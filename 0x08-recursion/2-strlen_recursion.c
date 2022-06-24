737403891e598f7755813b135e9d7faba2c38adc
MaryBlessing <marybngozi@gmail.com>
2022-06-24 09:53:23 +0000

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,10 @@
+#include <string.h>
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to print
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
