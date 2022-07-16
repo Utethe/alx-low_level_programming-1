1574443927e449c56f1eee6743c8e8ef2efdaebb
MaryBlessing <marybngozi@gmail.com>
2022-07-16 09:58:24 +0000

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,63 +1,26 @@
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _strlen - get the length of a string
- * @str: string to count
- * Return: int, length of the string
- */
-int _strlen(char *str)
-{
-	int c;
-
-	c = 0;
-	while (str[c] != '\0')
-	{
-		c++;
-	}
-	return (c);
-}
-
-/**
- * string_nconcat - concatenates 2 strings
- * @s1: string 1 to concatenate
- * @s2: string 2 to concatenate
- * @n: number of string from 2 to concatenate
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: length of the array
+ * @size: size of each element of the array
  * Return: a pointer to the allocated memory or NULL otherwise
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	int k, m, i, j;
-	unsigned int w;
+	unsigned int f;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	w = _strlen(s2);
-	if (n >= w)
-		m = w;
-	else
-		m = n;
-	k = _strlen(s1) + m;
-	ptr = malloc(sizeof(char) * (k + 1));
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
-	j = i = 0;
-	while (s1[j] != '\0')
+	f = 0;
+	while (f < nmemb)
 	{
-		ptr[i] = s1[j];
-		j++;
-		i++;
+		ptr[f] = 0;
+		f++;
 	}
-	j = 0;
-	while (j < m)
-	{
-		ptr[i] = s2[j];
-		i++;
-		j++;
-	}
-	ptr[i] = '\0';
 	return (ptr);
 }
