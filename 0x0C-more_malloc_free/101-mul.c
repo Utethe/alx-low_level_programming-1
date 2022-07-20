2d7adde2dc0b98446545d482066c6b0bf5c8723d
MaryBlessing <marybngozi@gmail.com>
2022-07-20 00:56:26 +0000

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - multiplies 2 numbers
+ * @argc: number of arguments
+ * @argv: arguments array
+ * Return: 0
+ */
+void main(int argc, char **argv)
+{
+	_putchar('0');
+}
