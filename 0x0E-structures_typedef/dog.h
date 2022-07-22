6a4d74c6b9cb2bac4e072bafc60a13aedb3ca40a
MaryBlessing <marybngozi@gmail.com>
2022-07-22 08:03:23 +0000

    previouses

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - for a dog definition
+ * @name: char* - name of the dog 
+ * @age: float - age of the dog 
+ * @owner: char* - owner of the dog 
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
