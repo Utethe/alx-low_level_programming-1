435b8d5bd72f07ec10c5a98cbe2e7cf355d02bf9
MaryBlessing <marybngozi@gmail.com>
2022-08-01 17:58:04 +0000

    0-binary_to_uint.c_finally

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,3 +1,4 @@
+#include <string.h>
 /**
  * binary_to_uint - converts binary number to unsigned int
  * @b: binary to convert
@@ -5,16 +6,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int w;
+	int w, p = 0;
 	unsigned int res = 0;
 
 	if (!b)
 		return (0);
-	for (w = 0; b[w]; w++)
+	for (w = strlen(b) - 1; w >= 0; w--)
 	{
 		if (b[w] < '0' || b[w] > '1')
 			return (0);
-		res = 2 * res + (b[w] - '0');
+		if (b[w] == '0')
+		{
+			p++;
+			continue;
+		}
+		res = res + ((b[w] - '0') << p);
+		p++;
 	}
 	return (res);
 }
